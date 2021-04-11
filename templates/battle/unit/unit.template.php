133f0ef067f5fdab75f4f6f07987cc622a17e0b3
WalkWeb <av18775@list.ru>
2021-04-11 11:10:13 +0300

    Update visual desing unit

--- templates/battle/unit/unit.template.php
+++ templates/battle/unit/unit.template.php
@@ -7,32 +7,71 @@ if (empty($unit) || !($unit instanceof UnitInterface)) {
     throw new ViewException(ViewException::MISSING_UNIT);
 }
 
+// todo добавить отображение уровня
+// todo добавить отображение иконки класса
+// todo добавить отображение длины полоски концентрации
+// todo добавить отображение длины полоски ярости
+
 ?>
-<div class="unit">
-    <table>
-        <tr>
-            <td rowspan="2" class="avatar_box" style="background-image: url(<?= $unit->getAvatar() ?>"></td>
-            <td>Name</td>
-            <td>Damage</td>
-            <td>Life</td>
-            <td>Cons</td>
-            <td>Action?</td>
-            <td>Alive?</td>
-        </tr>
-        <tr>
-            <td><?= $unit->getName() ?></td>
-            <td><?= $unit->getDamage() ?></td>
-            <td class="life_bar">
-                <div class="life_bar">
-                    <div class="life" style="width: <?= $this->getWidth($unit->getLife(), $unit->getTotalLife()) ?>%;"></div>
+<div align="center">
+    <div class="unit_main_box" id="usr_40">
+        <div class="unit_box1">
+            <div class="unit_box1_right">
+                <div class="unit_box1_right2">
+                    <div class="unit_box1_right3">
+                        <div class="unit_box1_right4">
+                            <div class="unit_hp">
+                                <div id="hp_bar_bg_40" class="unit_hp_bar">
+                                    <div id="hp_bar_40" class="unit_hp_bar2" style="width: <?= $this->getWidth($unit->getLife(), $unit->getTotalLife()) ?>%;"></div>
+                                </div>
+                                <div class="unit_hp_text">
+                                    <span class="hp"><?= $unit->getLife() ?></span> / <span class="thp"><?= $unit->getTotalLife() ?></span>
+                                </div>
+                                <div class="unit_hp_text_add">
+                                    <span class="recdam"></span>
+                                </div>
+                            </div>
+                            <div class="unit_cons">
+                                <div class="unit_cons_bar2" style="width: 100%;"></div>
+                            </div>
+                            <div class="unit_rage">
+                                <div class="unit_rage_bar2" style="width: 100%;"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="life_text">
-                    <?= $unit->getLife() . '/' . $unit->getTotalLife() ?>
+            </div>
+            <div class="unit_box1_left">
+                <div class="unit_box1_left2">
+                    <div class="unit_ava" style="background: url(<?= $unit->getAvatar() ?>) no-repeat center; background-size: cover;">
+                        <div id="ava40" class="unit_ava_blank"></div>
+                        <div id="avas40" class="unit_ava_blank"></div>
+                    </div>
                 </div>
-            </td>
-            <td><?= $unit->getConcentration() . '/' . UnitInterface::MAX_CONS ?></td>
-            <td class="<?= $this->getBgClass($unit->isAction()) ?>"><?= ($unit->isAction() ? 'yes' : 'no') ?></td>
-            <td><?= ($unit->isAlive() ? 'yes' : 'no') ?></td>
-        </tr>
-    </table>
+            </div>
+        </div>
+        <div class="unit_box2">
+            <div class="unit_box2_right">
+                <div class="unit_box2_right2">
+                    <div class="unit_box2_right3">
+                        <p><span class="unitcolor5"><?= $unit->getName() ?></span></p>
+                    </div>
+                </div>
+                <div class="unit_effect_contant">
+                    <p id="unit_effects_40"></p>
+                </div>
+            </div>
+            <div class="unit_box2_left">
+                <div class="unit_icon">
+                    <div class="unit_icon_left">
+                        1
+                    </div>
+                    <div class="unit_icon_right">
+                        <img src="/images/icons/class-icon-paladin.png" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
