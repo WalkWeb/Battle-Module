4d2914aafa3f2de63cc6071f8909ce49bfa1fd52
WalkWeb <av18775@list.ru>
2021-06-20 20:41:14 +0300

    Small improvement

--- templates/battle/head.template.php
+++ templates/battle/head.template.php
@@ -2,7 +2,7 @@
 <head>
     <title>Battle Module</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" type="text/css" href="/styles/main.css">
+    <link rel="stylesheet" type="text/css" href="/styles/battle.css">
     <script src="/js/battle.js"></script>
 </head>
 <body>
--- tests/Battle/View/ViewTest.php
+++ tests/Battle/View/ViewTest.php
@@ -326,10 +326,8 @@ EOT;
 <head>
     <title>Battle Module</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" type="text/css" href="/styles/main.css">
+    <link rel="stylesheet" type="text/css" href="/styles/battle.css">
     <script src="/js/battle.js"></script>
-    <!-- TODO jquery.js используется только для работы спойлера - можно уйти от этой зависимости -->
-    <script src="/js/jquery.js"></script>
 </head>
 <body>
 EOT;
