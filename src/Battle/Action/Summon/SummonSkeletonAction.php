2d7bcdf58374aa4a30c6acab3f98113ca9b10c6c
WalkWeb <av18775@list.ru>
2021-07-04 17:00:00 +0300

    Added SummonSkeletonAction

---
+++ src/Battle/Action/Summon/SummonSkeletonAction.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Battle\Action\Summon;
+
+use Battle\Unit\Race\RaceFactory;
+use Battle\Unit\Unit;
+use Battle\Unit\UnitInterface;
+use Exception;
+
+class SummonSkeletonAction extends SummonAction
+{
+    public const NAME = 'summon Skeleton';
+
+    private $name = 'Skeleton';
+    private $level = 1;
+    private $url = '/images/avas/monsters/003.png';
+    private $damage = 16;
+    private $attackSpeed = 1;
+    private $life = 38;
+    private $melee = true;
+    private $raceId = 8;
+
+    /**
+     * @return UnitInterface
+     * @throws Exception
+     */
+    public function getSummonUnit(): UnitInterface
+    {
+        if ($this->summonUnit === null) {
+            $this->summonUnit = new Unit(
+                self::generateId(),
+                $this->name,
+                $this->level,
+                $this->url,
+                $this->damage,
+                $this->attackSpeed,
+                $this->life,
+                $this->life,
+                $this->melee,
+                $this->actionUnit->getCommand(),
+                RaceFactory::create($this->raceId),
+                $this->message
+            );
+        }
+
+        return $this->summonUnit;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameAction(): string
+    {
+        return self::NAME;
+    }
+}
--- tests/Battle/Action/Summon/SummonSkeletonMageTest.php
+++ tests/Battle/Action/Summon/SummonSkeletonMageTest.php
@@ -27,5 +27,17 @@ class SummonSkeletonMageTest extends TestCase
         $action = new SummonSkeletonMage($actionUnit, $enemyCommand, $actionCommand, new Message());
 
         self::assertEquals(SummonSkeletonMage::NAME, $action->getNameAction());
+
+        $unit = $action->getSummonUnit();
+
+        self::assertEquals('Skeleton Mage', $unit->getName());
+        self::assertEquals(2, $unit->getLevel());
+        self::assertEquals('/images/avas/monsters/008.png', $unit->getAvatar());
+        self::assertEquals(13, $unit->getDamage());
+        self::assertEquals(1.2, $unit->getAttackSpeed());
+        self::assertEquals(42, $unit->getLife());
+        self::assertEquals(42, $unit->getTotalLife());
+        self::assertFalse($unit->isMelee());
+        self::assertEquals(8, $unit->getRace()->getId());
     }
 }
