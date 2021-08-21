7954333f8f408c3bee0787030332507e3c30abc9
WalkWeb <av18775@list.ru>
2021-08-21 21:44:32 +0300

    Added HealingPotionAbility

---
+++ src/Battle/Unit/Ability/Effect/HealingPotionAbility.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Battle\Unit\Ability\Effect;
+
+use Battle\Action\ActionCollection;
+use Battle\Action\EffectAction;
+use Battle\Action\HealAction;
+use Battle\Command\CommandInterface;
+use Battle\Unit\Ability\AbstractAbility;
+use Battle\Unit\Effect\Effect;
+use Battle\Unit\Effect\EffectCollection;
+use Battle\Unit\UnitInterface;
+
+class HealingPotionAbility extends AbstractAbility
+{
+    private const NAME        = 'Healing Potion';
+    private const ICON        = '/images/icons/ability/234.png';
+    private const USE_MESSAGE = 'use Healing Potion';
+    private const DURATION    = 4;
+    private const POWER       = 15;
+
+    /**
+     * @var ActionCollection
+     */
+    private $actions;
+
+    public function getAction(CommandInterface $enemyCommand, CommandInterface $alliesCommand): ActionCollection
+    {
+        return $this->createEffectActions($enemyCommand, $alliesCommand);
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getIcon(): string
+    {
+        return self::ICON;
+    }
+
+    /**
+     * Способность активируется при полной концентрации юнита
+     *
+     * @param UnitInterface $unit
+     */
+    public function update(UnitInterface $unit): void
+    {
+        if (!$this->ready && $unit->getConcentration() === UnitInterface::MAX_CONS) {
+            $this->ready = true;
+        }
+    }
+
+    /**
+     * Способность отмечает свое использование - переходит в неактивный статус и обнуляет концентрацию у юнита
+     */
+    public function usage(): void
+    {
+        $this->ready = false;
+        $this->unit->useConcentrationAbility();
+    }
+
+    /**
+     * Может ли способность быть применена - если аналогичный эффект существует, то нет
+     *
+     * @param CommandInterface $enemyCommand
+     * @param CommandInterface $alliesCommand
+     * @return bool
+     */
+    public function canByUsed(CommandInterface $enemyCommand, CommandInterface $alliesCommand): bool
+    {
+        foreach ($this->getAction($enemyCommand, $alliesCommand) as $action) {
+            if (!$action->canByUsed()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Создает коллекцию эффектов, которая будет применена к юниту
+     *
+     * @param CommandInterface $enemyCommand
+     * @param CommandInterface $alliesCommand
+     * @return ActionCollection
+     */
+    private function createEffectActions(CommandInterface $enemyCommand, CommandInterface $alliesCommand): ActionCollection
+    {
+        if ($this->actions === null) {
+            // Создаем коллекцию событий (с одним бафом), которая будет применена к персонажу, при применении эффекта
+            $onNextRoundActions = new ActionCollection();
+
+            $onNextRoundActions->add(new HealAction(
+                $this->unit,
+                $enemyCommand,
+                $alliesCommand,
+                self::POWER
+            ));
+
+            // Создаем коллекцию эффектов, с одним эффектом при применении - Reserve Forces
+            $effects = new EffectCollection();
+
+            // Создаем сам эффект
+            $effects->add(new Effect(
+                self::NAME,
+                self::ICON,
+                self::DURATION,
+                new ActionCollection(),
+                $onNextRoundActions,
+                new ActionCollection()
+            ));
+
+            // Создаем коллекцию событий для применения к юниту
+            $this->actions = new ActionCollection();
+
+            $this->actions->add(new EffectAction(
+                $this->unit,
+                $enemyCommand,
+                $alliesCommand,
+                self::USE_MESSAGE,
+                $effects
+            ));
+        }
+
+        return $this->actions;
+    }
+}
---
+++ tests/Battle/Unit/Ability/Effect/HealingPotionAbilityTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Battle\Unit\Ability\Effect;
+
+use Battle\Command\CommandFactory;
+use Battle\Unit\Ability\AbilityCollection;
+use Battle\Unit\Ability\Effect\HealingPotionAbility;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Tests\Battle\Factory\UnitFactory;
+
+class HealingPotionAbilityTest extends TestCase
+{
+    /**
+     * Тест на создание способности HealingPotionAbility
+     *
+     * @throws Exception
+     */
+    public function testHealingPotionAbilityCreate(): void
+    {
+        $name = 'Healing Potion';
+        $icon = '/images/icons/ability/234.png';
+        $unit = UnitFactory::createByTemplate(1);
+        $enemyUnit = UnitFactory::createByTemplate(2);
+        $command = CommandFactory::create([$unit]);
+        $enemyCommand = CommandFactory::create([$enemyUnit]);
+
+        $ability = new HealingPotionAbility($unit);
+
+        self::assertEquals($name, $ability->getName());
+        self::assertEquals($icon, $ability->getIcon());
+        self::assertEquals($unit, $ability->getUnit());
+        self::assertFalse($ability->isReady());
+        self::assertTrue($ability->canByUsed($enemyCommand, $command));
+
+        // Up concentration
+        for ($i = 0; $i < 10; $i++) {
+            $unit->newRound();
+        }
+
+        $collection = new AbilityCollection();
+        $collection->add($ability);
+
+        foreach ($collection as $item) {
+            self::assertEquals($ability, $item);
+        }
+
+        $collection->update($unit);
+
+        self::assertTrue($ability->isReady());
+
+        $ability->usage();
+
+        self::assertFalse($ability->isReady());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testHealingPotionAbilityApply(): void
+    {
+        $unit = UnitFactory::createByTemplate(11);
+        $enemyUnit = UnitFactory::createByTemplate(2);
+        $command = CommandFactory::create([$unit]);
+        $enemyCommand = CommandFactory::create([$enemyUnit]);
+
+        $ability = new HealingPotionAbility($unit);
+
+        foreach ($ability->getAction($enemyCommand, $command) as $action) {
+            $action->handle();
+        }
+
+        self::assertCount(1, $unit->getEffects());
+
+        self::assertEquals(1, $unit->getLife());
+
+        $unit->newRound();
+
+        self::assertEquals(1 + 15, $unit->getLife());
+
+        $unit->newRound();
+
+        self::assertEquals(1 + 30, $unit->getLife());
+
+        $unit->newRound();
+
+        self::assertEquals(1 + 45, $unit->getLife());
+
+        $unit->newRound();
+
+        self::assertEquals(1 + 60, $unit->getLife());
+
+        $unit->newRound();
+
+        self::assertEquals(1 + 60, $unit->getLife());
+
+        self::assertCount(0, $unit->getEffects());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testHealingPotionAbilityCanBeUsed(): void
+    {
+        $unit = UnitFactory::createByTemplate(11);
+        $enemyUnit = UnitFactory::createByTemplate(2);
+        $command = CommandFactory::create([$unit]);
+        $enemyCommand = CommandFactory::create([$enemyUnit]);
+
+        $ability = new HealingPotionAbility($unit);
+
+        // Перед применением способности эффекта на юните еще нет - способность может быть применена
+        self::assertTrue($ability->canByUsed($enemyCommand, $command));
+
+        foreach ($ability->getAction($enemyCommand, $command) as $action) {
+            $action->handle();
+        }
+
+        // После появления эффекта на юните - способность уже не может быть применена
+        self::assertFalse($ability->canByUsed($enemyCommand, $command));
+
+        // Пропускаем ходы
+        for ($i = 0; $i < 10; $i++) {
+            $unit->newRound();
+        }
+
+        // Эффект исчез - способность опять может быть применена
+        self::assertTrue($ability->canByUsed($enemyCommand, $command));
+    }
+}
