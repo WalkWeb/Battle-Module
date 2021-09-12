431e515f5d59db84ca25be1b6cf934db78953ef3
WalkWeb <av18775@list.ru>
2021-09-12 20:59:41 +0300

    Added PoisonAbility

---
+++ tests/Battle/Unit/Ability/Effect/PoisonAbilityTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Battle\Unit\Ability\Effect;
+
+use Battle\Action\ActionCollection;
+use Battle\Action\ActionFactory;
+use Battle\Action\ActionInterface;
+use Battle\Action\DamageAction;
+use Battle\Command\CommandFactory;
+use Battle\Command\CommandInterface;
+use Battle\Unit\Ability\AbilityCollection;
+use Battle\Unit\Ability\Effect\PoisonAbility;
+use Battle\Unit\UnitInterface;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Tests\Battle\Factory\UnitFactory;
+
+class PoisonAbilityTest extends TestCase
+{
+    /**
+     * Тест на создание способности PoisonAbility
+     *
+     * @throws Exception
+     */
+    public function testPoisonAbilityCreate(): void
+    {
+        $name = 'Poison';
+        $icon = '/images/icons/ability/202.png';
+        $unit = UnitFactory::createByTemplate(1);
+        $enemyUnit = UnitFactory::createByTemplate(2);
+        $command = CommandFactory::create([$unit]);
+        $enemyCommand = CommandFactory::create([$enemyUnit]);
+
+        $ability = new PoisonAbility($unit);
+
+        self::assertEquals($name, $ability->getName());
+        self::assertEquals($icon, $ability->getIcon());
+        self::assertEquals($unit, $ability->getUnit());
+        self::assertFalse($ability->isReady());
+        self::assertTrue($ability->canByUsed($enemyCommand, $command));
+
+        // Up concentration
+        for ($i = 0; $i < 10; $i++) {
+            $unit->newRound();
+        }
+
+        $collection = new AbilityCollection();
+        $collection->add($ability);
+
+        foreach ($collection as $item) {
+            self::assertEquals($ability, $item);
+        }
+
+        $collection->update($unit);
+
+        self::assertTrue($ability->isReady());
+
+        $ability->usage();
+
+        self::assertFalse($ability->isReady());
+    }
+
+    /**
+     * Тест на получение Actions из PoisonAbility
+     *
+     * @throws Exception
+     */
+    public function testPoisonAbilityGetActions(): void
+    {
+        $unit = UnitFactory::createByTemplate(1);
+        $enemyUnit = UnitFactory::createByTemplate(2);
+        $command = CommandFactory::create([$unit]);
+        $enemyCommand = CommandFactory::create([$enemyUnit]);
+
+        $ability = new PoisonAbility($unit);
+
+        self::assertEquals(
+            $this->createActions($unit, $command, $enemyCommand),
+            $ability->getAction($enemyCommand, $command)
+        );
+    }
+
+    /**
+     * @param UnitInterface $unit
+     * @param CommandInterface $command
+     * @param CommandInterface $enemyCommand
+     * @return ActionCollection
+     * @throws Exception
+     */
+    private function createActions(
+        UnitInterface $unit,
+        CommandInterface $command,
+        CommandInterface $enemyCommand
+    ): ActionCollection
+    {
+        $actionFactory = new ActionFactory();
+
+        $data = [
+            'type'           => ActionInterface::EFFECT,
+            'action_unit'    => $unit,
+            'enemy_command'  => $enemyCommand,
+            'allies_command' => $command,
+            'type_target'    => ActionInterface::TARGET_RANDOM_ENEMY,
+            'name'           => 'use Poison',
+            'effects'        => [
+                [
+                    'name'                  => 'Poison',
+                    'icon'                  => '/images/icons/ability/202.png',
+                    'duration'              => 5,
+                    'on_apply_actions'      => [],
+                    'on_next_round_actions' => [
+                        [
+                            'type'             => ActionInterface::DAMAGE,
+                            'action_unit'      => $unit,
+                            'enemy_command'    => $enemyCommand,
+                            'allies_command'   => $command,
+                            'type_target'      => ActionInterface::TARGET_SELF,
+                            'name'             => null,
+                            'power'            => 8,
+                            'animation_method' => DamageAction::EFFECT_ANIMATION_METHOD,
+                        ],
+                    ],
+                    'on_disable_actions'    => [],
+                ],
+            ],
+        ];
+
+        $actions = new ActionCollection();
+        $actions->add($actionFactory->create($data));
+
+        return $actions;
+    }
+}
