dd73b8c80039cf4c73ffa9b57b6cb7a2cff8e4dc
WalkWeb <av18775@list.ru>
2022-01-08 17:15:04 +0300

    Refactoring CommandTest

--- tests/Battle/Command/CommandTest.php
+++ tests/Battle/Command/CommandTest.php
@@ -10,6 +10,7 @@ use Battle\Action\EffectAction;
 use Battle\Command\CommandInterface;
 use Battle\Unit\Effect\EffectFactory;
 use Battle\Unit\Effect\EffectInterface;
+use Battle\Unit\UnitException;
 use Exception;
 use Battle\Battle;
 use Battle\Container\Container;
@@ -548,6 +549,37 @@ class CommandTest extends AbstractUnitTest
         self::assertEquals($unit, $command->getUnitForEffectHeal($effect));
     }
 
+    /**
+     * @throws CommandException
+     * @throws UnitException
+     * @throws Exception
+     */
+    public function testGetUnitForHealNull(): void
+    {
+        $unit = UnitFactory::createByTemplate(10);
+
+        $unitCollection = new UnitCollection();
+        $unitCollection->add($unit);
+        $command = new Command($unitCollection);
+
+        self::assertNull($command->getUnitForHeal());
+    }
+
+    /**
+     * Тест на выбор самого раненого юнита из команды для лечения
+     *
+     * @dataProvider unitDataProvider
+     * @param array $data
+     * @param string $unitIdForHeal
+     * @throws CommandException
+     * @throws UnitException
+     */
+    public function testGetMostWoundedUnitForHeal(array $data, string $unitIdForHeal): void
+    {
+        $command = CommandFactory::create($data);
+        self::assertEquals($unitIdForHeal, $command->getUnitForHeal()->getId());
+    }
+
     // ------------------------------ Тест на метод getUnitForResurrection() -------------------------------------------
     // Проверены следующие ситуации:
     // 1. Один живой юнит => null
@@ -580,6 +612,78 @@ class CommandTest extends AbstractUnitTest
         $command = CommandFactory::create([$unit, $deadUnit]);
 
         self::assertEquals($deadUnit, $command->getUnitForResurrection());
+    }
+
+    /**
+     * @return array
+     */
+    public function unitDataProvider(): array
+    {
+        return [
+            [
+                [
+                    [
+                        'id'           => '63ad76c6-6a11-44ef-997b-fea1778bebe5',
+                        'name'         => 'Warrior',
+                        'level'        => 1,
+                        'avatar'       => 'url avatar 1',
+                        'damage'       => 15,
+                        'attack_speed' => 1.2,
+                        'life'         => 70,
+                        'total_life'   => 80,
+                        'melee'        => true,
+                        'class'        => 1,
+                        'race'         => 1,
+                        'command'      => 1,
+                    ],
+                    // Самый битый юнит - именно он должен выбираться для лечения
+                    [
+                        'id'           => 'fb8be211-0782-4c60-8865-68b177ffbe0c',
+                        'name'         => 'Knight',
+                        'level'        => 1,
+                        'avatar'       => 'url avatar 2',
+                        'damage'       => 11,
+                        'attack_speed' => 0.9,
+                        'life'         => 12,
+                        'total_life'   => 115,
+                        'melee'        => false,
+                        'class'        => 1,
+                        'race'         => 1,
+                        'command'      => 1,
+                    ],
+                    [
+                        'id'           => 'fb8be211-0782-4c60-8865-68b177ffbedc',
+                        'name'         => 'Archer',
+                        'level'        => 1,
+                        'avatar'       => 'url avatar 3',
+                        'damage'       => 11,
+                        'attack_speed' => 0.9,
+                        'life'         => 50,
+                        'total_life'   => 75,
+                        'melee'        => false,
+                        'class'        => 2,
+                        'race'         => 1,
+                        'command'      => 1,
+                    ],
+                    // Мертвый юнит - он выбираться не должен
+                    [
+                        'id'           => 'fb8be211-0782-4c60-8865-68b177ffbe3c',
+                        'name'         => 'Priest',
+                        'level'        => 1,
+                        'avatar'       => 'url avatar 3',
+                        'damage'       => 11,
+                        'attack_speed' => 0.9,
+                        'life'         => 0,
+                        'total_life'   => 60,
+                        'melee'        => false,
+                        'class'        => 2,
+                        'race'         => 1,
+                        'command'      => 1,
+                    ],
+                ],
+                'fb8be211-0782-4c60-8865-68b177ffbe0c',
+            ],
+        ];
     }
 
     /**
