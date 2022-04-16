83ab3f0e68e802372dc427bebbac91204a371376
WalkWeb <av18775@list.ru>
2022-04-16 19:48:12 +0300

    Improved Defense class

--- src/Battle/Unit/Defense/Defense.php
+++ src/Battle/Unit/Defense/Defense.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Battle\Unit\Defense;
 
-// TODO В сеттерах добавить проверки на допустимое значение
-
 class Defense implements DefenseInterface
 {
     /**
@@ -34,9 +32,16 @@ class Defense implements DefenseInterface
 
     /**
      * @param int $defense
+     * @throws DefenseException
      */
     public function setDefense(int $defense): void
     {
+        if ($defense < self::MIN_DEFENSE || $defense > self::MAX_DEFENSE) {
+            throw new DefenseException(
+                DefenseException::INCORRECT_DEFENSE_VALUE . DefenseInterface::MIN_DEFENSE . '-' . DefenseInterface::MAX_DEFENSE
+            );
+        }
+
         $this->defense = $defense;
     }
 
@@ -50,9 +55,16 @@ class Defense implements DefenseInterface
 
     /**
      * @param int $block
+     * @throws DefenseException
      */
     public function setBlock(int $block): void
     {
+        if ($block < self::MIN_BLOCK || $block > self::MAX_BLOCK) {
+            throw new DefenseException(
+                DefenseException::INCORRECT_BLOCK_VALUE . DefenseInterface::MIN_BLOCK . '-' . DefenseInterface::MAX_BLOCK
+            );
+        }
+
         $this->block = $block;
     }
 }
--- tests/Battle/Unit/Defense/DefenseTest.php
+++ tests/Battle/Unit/Defense/DefenseTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Tests\Battle\Unit\Defense;
 
 use Battle\Unit\Defense\Defense;
+use Battle\Unit\Defense\DefenseException;
+use Battle\Unit\Defense\DefenseInterface;
 use Tests\AbstractUnitTest;
 
 class DefenseTest extends AbstractUnitTest
@@ -25,6 +27,8 @@ class DefenseTest extends AbstractUnitTest
 
     /**
      * Тест на обновление параметров Defense
+     *
+     * @throws DefenseException
      */
     public function testDefenseUpdate(): void
     {
@@ -35,5 +39,69 @@ class DefenseTest extends AbstractUnitTest
 
         self::assertEquals($defenseValue, $defense->getDefense());
         self::assertEquals($block, $defense->getBlock());
+    }
+
+    /**
+     * Тест на ошибку, когда в Defense пытаются записать слишком низкое значение защиты
+     *
+     * @throws DefenseException
+     */
+    public function testDefenseSetUltraMinDefense(): void
+    {
+        $defense = new Defense(10, 10);
+
+        $this->expectException(DefenseException::class);
+        $this->expectExceptionMessage(
+            DefenseException::INCORRECT_DEFENSE_VALUE . DefenseInterface::MIN_DEFENSE . '-' . DefenseInterface::MAX_DEFENSE
+        );
+        $defense->setDefense(DefenseInterface::MIN_DEFENSE - 1);
+    }
+
+    /**
+     * Тест на ошибку, когда в Defense пытаются записать слишком большое значение защиты
+     *
+     * @throws DefenseException
+     */
+    public function testDefenseSetUltraMaxDefense(): void
+    {
+        $defense = new Defense(10, 10);
+
+        $this->expectException(DefenseException::class);
+        $this->expectExceptionMessage(
+            DefenseException::INCORRECT_DEFENSE_VALUE . DefenseInterface::MIN_DEFENSE . '-' . DefenseInterface::MAX_DEFENSE
+        );
+        $defense->setDefense(DefenseInterface::MAX_DEFENSE + 1);
+    }
+
+    /**
+     * Тест на ошибку, когда в Defense пытаются записать слишком низкое значение блока
+     *
+     * @throws DefenseException
+     */
+    public function testDefenseSetUltraMinBlock(): void
+    {
+        $defense = new Defense(10, 10);
+
+        $this->expectException(DefenseException::class);
+        $this->expectExceptionMessage(
+            DefenseException::INCORRECT_BLOCK_VALUE . DefenseInterface::MIN_BLOCK . '-' . DefenseInterface::MAX_BLOCK
+        );
+        $defense->setBlock(DefenseInterface::MIN_BLOCK - 1);
+    }
+
+    /**
+     * Тест на ошибку, когда в Defense пытаются записать слишком большое значение блока
+     *
+     * @throws DefenseException
+     */
+    public function testDefenseSetUltraMaxBlock(): void
+    {
+        $defense = new Defense(10, 10);
+
+        $this->expectException(DefenseException::class);
+        $this->expectExceptionMessage(
+            DefenseException::INCORRECT_BLOCK_VALUE . DefenseInterface::MIN_BLOCK . '-' . DefenseInterface::MAX_BLOCK
+        );
+        $defense->setBlock(DefenseInterface::MAX_BLOCK + 1);
     }
 }
