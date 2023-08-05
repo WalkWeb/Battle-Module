a3d158c6c4a7e54c2774189776f5d7f4aa493b31
WalkWeb <av18775@list.ru>
2023-08-05 14:42:27 +0300

    Unit: moved modify stats methods to trait

--- src/Battle/Unit/Unit.php
+++ src/Battle/Unit/Unit.php
@@ -12,14 +12,14 @@ use Battle\Action\WaitAction;
 use Battle\Command\CommandInterface;
 use Battle\Unit\Ability\AbilityCollection;
 use Battle\Unit\Ability\AbilityInterface;
-use Battle\Unit\Defense\DefenseException;
-use Battle\Unit\Defense\DefenseInterface;
-use Battle\Unit\Offense\OffenseException;
 use Battle\Unit\Offense\OffenseInterface;
+use Battle\Unit\Traits\ModifyStatsTrait;
 use Exception;
 
 class Unit extends AbstractUnit
 {
+    use ModifyStatsTrait;
+
     /**
      * Возвращает абстрактное действие (действия) от юнита в его ходе.
      *
@@ -307,270 +307,6 @@ class Unit extends AbstractUnit
     }
 
     /**
-     * Увеличивает урон юнита (можно сделать и уменьшение, но пока делаем только увеличение)
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws UnitException
-     * @throws OffenseException
-     */
-    private function multiplierPhysicalDamage(ActionInterface $action): void
-    {
-        if ($action->getPower() <= 100) {
-            throw new UnitException(UnitException::NO_REDUCED_DAMAGE);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $oldDamage = $this->offense->getPhysicalDamage();
-        $newDamage = (int)($this->offense->getPhysicalDamage() * $multiplier);
-
-        $bonus = $newDamage - $oldDamage;
-
-        $this->offense->setPhysicalDamage($newDamage);
-
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает изменение урона юнита
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws OffenseException
-     */
-    private function multiplierPhysicalDamageRevert(ActionInterface $action): void
-    {
-        $this->offense->setPhysicalDamage($this->offense->getPhysicalDamage() - $action->getRevertValue());
-    }
-
-    /**
-     * Изменяет меткость юнита
-     *
-     * @param ActionInterface $action
-     * @throws ActionException
-     * @throws OffenseException
-     * @throws UnitException
-     */
-    private function multiplierAccuracy(ActionInterface $action): void
-    {
-        if ($action->getPower() <= ActionInterface::MIN_MULTIPLIER) {
-            throw new UnitException(UnitException::OVER_REDUCED . ActionInterface::MIN_MULTIPLIER);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $oldAccuracy = $this->offense->getAccuracy();
-        $newAccuracy = (int)($this->offense->getAccuracy() * $multiplier);
-
-        $bonus = $newAccuracy - $oldAccuracy;
-
-        $this->offense->setAccuracy($newAccuracy);
-
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает изменение меткости
-     *
-     * @param ActionInterface $action
-     * @throws ActionException
-     * @throws OffenseException
-     */
-    private function multiplierAccuracyRevert(ActionInterface $action): void
-    {
-        $this->offense->setAccuracy($this->offense->getAccuracy() - $action->getRevertValue());
-    }
-
-    /**
-     * Изменяет магическую меткость юнита
-     *
-     * @param ActionInterface $action
-     * @throws ActionException
-     * @throws OffenseException
-     * @throws UnitException
-     */
-    private function multiplierMagicAccuracy(ActionInterface $action): void
-    {
-        if ($action->getPower() <= ActionInterface::MIN_MULTIPLIER) {
-            throw new UnitException(UnitException::OVER_REDUCED . ActionInterface::MIN_MULTIPLIER);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $oldMagicAccuracy = $this->offense->getMagicAccuracy();
-        $newMagicAccuracy = (int)($this->offense->getMagicAccuracy() * $multiplier);
-
-        $bonus = $newMagicAccuracy - $oldMagicAccuracy;
-
-        $this->offense->setMagicAccuracy($newMagicAccuracy);
-
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает изменение магической меткости
-     *
-     * @param ActionInterface $action
-     * @throws ActionException
-     * @throws OffenseException
-     */
-    private function multiplierMagicAccuracyRevert(ActionInterface $action): void
-    {
-        $this->offense->setMagicAccuracy($this->offense->getMagicAccuracy() - $action->getRevertValue());
-    }
-
-    /**
-     * Изменяет защиту юнита
-     *
-     * @param ActionInterface $action
-     * @throws Exception
-     */
-    private function multiplierDefense(ActionInterface $action): void
-    {
-        if ($action->getPower() <= ActionInterface::MIN_MULTIPLIER) {
-            throw new UnitException(UnitException::OVER_REDUCED . ActionInterface::MIN_MULTIPLIER);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $oldDefense = $this->defense->getDefense();
-        $newDefense = (int)($this->defense->getDefense() * $multiplier);
-
-        $bonus = $newDefense - $oldDefense;
-
-        $this->defense->setDefense($newDefense);
-
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает изменение защиты
-     *
-     * @param ActionInterface $action
-     * @throws Exception
-     */
-    private function multiplierDefenseRevert(ActionInterface $action): void
-    {
-        $this->defense->setDefense($this->defense->getDefense() - $action->getRevertValue());
-    }
-
-    /**
-     * Увеличивает здоровье юнита (можно сделать и уменьшение, но пока делаем только увеличение)
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws UnitException
-     * @throws ActionException
-     */
-    private function multiplierMaxLife(ActionInterface $action): void
-    {
-        if ($action->getPower() <= 100) {
-            throw new UnitException(UnitException::NO_REDUCED_MAXIMUM_LIFE);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $oldLife = $this->totalLife;
-        $newHpMax = (int)($this->totalLife * $multiplier);
-
-        $bonus = $newHpMax - $oldLife;
-
-        $this->life += $bonus;
-        $this->totalLife += $bonus;
-
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает изменения по здоровью
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     */
-    private function multiplierMaxLifeRevert(ActionInterface $action): void
-    {
-        $this->totalLife -= $action->getRevertValue();
-
-        if ($this->life > $this->totalLife) {
-            $this->life = $this->totalLife;
-        }
-    }
-
-    /**
-     * Увеличивает скорость атаки юнита
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws OffenseException
-     * @throws UnitException
-     */
-    private function multiplierAttackSpeed(ActionInterface $action): void
-    {
-        if ($action->getPower() <= 100) {
-            throw new UnitException(UnitException::NO_REDUCED_ATTACK_SPEED);
-        }
-
-        $multiplier = $action->getPower() / 100;
-
-        $attackSpeed = $this->offense->getAttackSpeed();
-        $oldAttackSpeed = $attackSpeed;
-        $newAttackSpeed = $attackSpeed * $multiplier;
-
-        $bonus = $newAttackSpeed - $oldAttackSpeed;
-
-        $this->offense->setAttackSpeed($newAttackSpeed);
-        $action->setRevertValue($bonus);
-    }
-
-    /**
-     * Откатывает обратно увеличенную скорость атаки
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws OffenseException
-     */
-    private function multiplierAttackSpeedRevert(ActionInterface $action): void
-    {
-        $attackSpeed = $this->offense->getAttackSpeed();
-        $attackSpeed -= $action->getRevertValue();
-        $this->offense->setAttackSpeed($attackSpeed);
-    }
-
-    /**
-     * Увеличивает блок юнита на фиксированную величину
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws DefenseException
-     */
-    private function addBlock(ActionInterface $action): void
-    {
-        $oldBlock = $block = $this->defense->getBlock();
-        $block += $action->getPower();
-
-        if ($block > DefenseInterface::MAX_BLOCK) {
-            $block = DefenseInterface::MAX_BLOCK;
-        }
-
-        $this->defense->setBlock($block);
-        $action->setRevertValue($block - $oldBlock);
-    }
-
-    /**
-     * Возвращает блок юнита к исходному значению
-     *
-     * @param ActionInterface $action - ожидается BuffAction
-     * @throws ActionException
-     * @throws DefenseException
-     */
-    private function addBlockRevert(ActionInterface $action): void
-    {
-        $block = $this->defense->getBlock();
-        $block -= $action->getRevertValue();
-        $this->defense->setBlock($block);
-    }
-
-    /**
      * @param CommandInterface $enemyCommand
      * @param CommandInterface $alliesCommand
      * @return ActionCollection
@@ -664,6 +400,7 @@ class Unit extends AbstractUnit
 
         // TODO Можно добавить шанс попадания в FullLog, для большей информативности логов
         // TODO Но реализовать одно сообщение при ударе нескольких целей - не так просто
+        // TODO Как вариант - добавлять сообщение в FullLog не от сюда, а из чата
         //$this->container->getFullLog()->add('<p>Шанс попадания: ' . $chanceOfHit . '%</p>');
 
         if ($chanceOfHit < self::MIN_HIT_CHANCE) {
